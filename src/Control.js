af4b7048b27b12b06dad925a3a16f8b386f7cf30
gayoung <did-619@naver.com>
2023-09-11 19:05:21 +0900

    delete 기능추가

--- src/Control.js
+++ src/Control.js
@@ -1,16 +1,23 @@
 'use client';
 import Link from "next/link";
-import {useParams} from "next/navigation";
+import {useParams, useRouter} from "next/navigation";
 
 export function Control(){
 	const params = useParams();
-	console.log(params)
+	const router = useRouter();
 	const id = params.id;
 	let contextUI = null;
 	if(id){
 		contextUI = <>
 			<li><Link href={`/update/${id}`}>update</Link></li>
-			<li><button>delete</button></li>
+			<li><button onClick={async ()=>{
+				const resp = await fetch('http://localhost:9999/topics/'+id,{
+					method: 'DELETE',
+				})
+				const topic = await resp.json();
+				router.push('/');
+				router.refresh();
+			}}>delete</button></li>
 		</>
 	}
 	return <>
